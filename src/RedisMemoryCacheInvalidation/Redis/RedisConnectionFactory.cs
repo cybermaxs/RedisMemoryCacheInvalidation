b7bc7c27542374855c32175dac31b5991ddfafcf
Maxime Lemaitre <maxime.lemaitre@outlook.com>
2015-08-24 08:59:56 +0200

    Add missing file

---
+++ src/RedisMemoryCacheInvalidation/Redis/RedisConnectionFactory.cs
@@ -0,0 +1,17 @@
+﻿using StackExchange.Redis;
+
+namespace RedisMemoryCacheInvalidation.Redis
+{
+    internal class RedisConnectionFactory
+    {
+        public static IRedisConnection New(IConnectionMultiplexer mux)
+        {
+            return new ExistingRedisConnnection(mux);
+        }
+
+        public static IRedisConnection New(ConfigurationOptions options)
+        {
+            return new StandaloneRedisConnection(options);
+        }
+    }
+}
