de90ee78322e652444336e43e4041bc3012370c9
Maxime Lemaitre <maxime.lemaitre@outlook.com>
2015-08-25 21:53:15 +0200

    Change xunit runner

--- test.ps1
+++ test.ps1
@@ -1,5 +1,5 @@
-./packages\OpenCover.4.6.166\tools\OpenCover.Console.exe -register:user -target:xunit.console.clr4.exe "-targetargs:""tests\RedisMemoryCacheInvalidation.Tests\bin\$env:CONFIGURATION\RedisMemoryCacheInvalidation.dll"" /noshadow /appveyor -notrait ""category=Integration""" -filter:"+[RedisMemoryCacheInvalidation*]*" -output:opencoverCoverage.xml
+./packages\OpenCover.4.6.166\tools\OpenCover.Console.exe -register:user -target:\xunit.console.x86 "-targetargs:""tests\RedisMemoryCacheInvalidation.Tests\bin\$env:CONFIGURATION\RedisMemoryCacheInvalidation.dll"" /noshadow /appveyor -notrait ""category=Integration""" -filter:"+[RedisMemoryCacheInvalidation*]*" -output:opencoverCoverage.xml
 
-$coveralls = (Resolve-Path "src/packages/coveralls.net.*/tools/csmacnz.coveralls.exe").ToString()
+$coveralls = (Resolve-Path "./packages/coveralls.net.*/tools/csmacnz.coveralls.exe").ToString()
 
 & $coveralls --opencover -i opencoverCoverage.xml --repoToken $env:COVERALLS_REPO_TOKEN --commitId $env:APPVEYOR_REPO_COMMIT --commitBranch $env:APPVEYOR_REPO_BRANCH --commitAuthor $env:APPVEYOR_REPO_COMMIT_AUTHOR --commitEmail $env:APPVEYOR_REPO_COMMIT_AUTHOR_EMAIL --commitMessage $env:APPVEYOR_REPO_COMMIT_MESSAGE --jobId $env:APPVEYOR_JOB_ID
