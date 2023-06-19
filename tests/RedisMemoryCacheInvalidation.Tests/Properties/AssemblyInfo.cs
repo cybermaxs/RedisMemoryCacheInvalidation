7fcc5ce85f1d67a9d2b541ada4e90de6f3569092
Peter Dolkens <peter.dolkens@clubspark.com>
2023-06-19 17:09:56 +0100

    Upgrade StackExchange.Redis to 2.2.88

--- tests/RedisMemoryCacheInvalidation.Tests/Properties/AssemblyInfo.cs
+++ tests/RedisMemoryCacheInvalidation.Tests/Properties/AssemblyInfo.cs
@@ -1,6 +1,7 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using Xunit;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -32,7 +33,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.1.58")]
-[assembly: AssemblyFileVersion("2.1.58")]
+[assembly: AssemblyVersion("2.2.88")]
+[assembly: AssemblyFileVersion("2.2.88")]
 
-
+[assembly: CollectionBehavior(DisableTestParallelization = true)]
